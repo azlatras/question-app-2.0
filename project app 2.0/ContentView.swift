41193e82d7a152a9e9ac0b6dfeb81e20dba4c726
azlatras <azlatras@gmail.com>
2023-06-28 00:01:56 -0400

    Initial Commit

---
+++ project app 2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  project app 2.0
+//
+//  Created by azille l on 6/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project app 2.0/project_app_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  project_app_2_0App.swift
+//  project app 2.0
+//
+//  Created by azille l on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct project_app_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
